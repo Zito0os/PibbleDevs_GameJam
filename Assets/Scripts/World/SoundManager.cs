221eba382acf6e53229eed30a990aa027bd09c1f
Oliver <olivercervantes2006@hotmail.com>
2026-05-17 00:19:34 -0600

    audios importados

---
+++ Assets/Scripts/World/SoundManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public enum SoundType
+{
+    Footstep,
+    Correr,
+    AttackPrincipal,
+    ClearLlegado,
+    FreezeLlegado,
+    SlowLlegado,
+    AbrirPuerta,
+    CerrarPuerta,
+    AbrirCofre,
+    CerrarCofre,
+    LlaveRota,
+    ErrorEleccion
+}
+
+[RequireComponent(typeof(AudioSource))]  
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(SoundType sound,float volume =1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+}
